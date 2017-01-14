5811cab7a46a5c269a69920e494e4c8834d7e53b
tournevis <arthur.ro92@gmail.com>
2017-01-14 13:59:16 +0100

    🐺 setup route and nav

--- src/router.js
+++ src/router.js
@@ -1,17 +1,17 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import App from './App.vue'
-import Home from './components/home.vue'
+import Home from './components/Home.vue'
+import Setup from './components/SetupBoop.vue'
 
 Vue.use(VueRouter)
 const routes = [
   {
     path: '/',
-    component: App
+    component: Home
   },
   {
-    path: '/home',
-    component: Home
+    path: '/setup',
+    component: Setup
   }
 ]
 const router = new VueRouter({
