40d4ed807d8ebbe2de4aae0edfb5f928074f8949
tournevis <arthur.ro92@gmail.com>
2017-01-14 14:02:13 +0100

    💎  add next  vue

---
+++ src/components/SetupBoop.vue
@@ -0,0 +1,24 @@
+<template lang="html">
+  <div id="SetupBoop">
+    <router-link to="/">
+      back
+    </router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name:'SetupBoop',
+  data () {
+    return {}
+  },
+  computed: {},
+  ready () {},
+  attached () {},
+  methods: {},
+  components: {}
+}
+</script>
+
+<style lang="css">
+</style>
